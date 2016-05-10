94e22882f8c81d6735ea1acc0c213ecacbaf0359
dwjdwj1216 <dwjdwj2@126.com>
2016-05-10 19:39:24 +0800

    Initial Commit

---
+++ HttpDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpDemo
+//
+//  Created by 戴王炯 on 5/10/16.
+//  Copyright © 2016 戴王炯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HttpDemoTests/HttpDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  HttpDemoTests.m
+//  HttpDemoTests
+//
+//  Created by 戴王炯 on 5/10/16.
+//  Copyright © 2016 戴王炯. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HttpDemoTests : XCTestCase
+
+@end
+
+@implementation HttpDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
